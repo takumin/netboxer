0e6326a642f74587091f66feae0e3645e758a8b0
takumin <takumiiinn@gmail.com>
2019-07-16 18:07:46 +0900

    impl getSites()

--- netboxer.go
+++ netboxer.go
@@ -3,14 +3,35 @@ package netboxer
 import (
 	"github.com/digitalocean/go-netbox/netbox"
 	"github.com/digitalocean/go-netbox/netbox/client"
+	"github.com/digitalocean/go-netbox/netbox/models"
+	"github.com/takumin/netboxer"
 )
+
+type NetBoxerSiteID int64
 
 type NetBoxer struct {
 	client *client.NetBox
+
+	sites map[NetBoxerSiteID]*models.Site
 }
 
 func NewNetboxer(endpointUrl, apiToken string) *NetBoxer {
-	return &NetBoxer{
-		client: netbox.NewNetboxWithAPIKey(endpointUrl, apiToken),
+	n := &NetBoxer{}
+	n.client = netbox.NewNetboxWithAPIKey(endpointUrl, apiToken)
+	n.sites = make(map[NetBoxerSiteID]*models.Site)
+	n.getSites()
+	return n
+}
+
+func (n *NetBoxer) getSites() error {
+	res, err := n.client.Dcim.DcimSitesList(nil, nil)
+	if err != nil {
+		return err
 	}
+
+	for _, v := range res.Payload.Results {
+		n.sites[NetBoxerSiteID(v.ID)] = v
+	}
+
+	return nil
 }
