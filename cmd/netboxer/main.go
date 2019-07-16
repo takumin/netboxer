46099089c6f8a9b9cb37feecb4459e16c7e076a9
takumin <takumiiinn@gmail.com>
2019-07-16 19:47:12 +0900

    fix golint

--- cmd/netboxer/main.go
+++ cmd/netboxer/main.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	defaultEndpointUrl = "127.0.0.1:32768"
-	defaultApiToken    = "0123456789abcdef0123456789abcdef01234567"
+	defaultEndpointURL = "127.0.0.1:32768"
+	defaultAPIToken    = "0123456789abcdef0123456789abcdef01234567"
 )
 
 func main() {
-	netboxer.NewNetboxer(defaultEndpointUrl, defaultApiToken)
+	netboxer.NewNetboxer(defaultEndpointURL, defaultAPIToken)
 }
